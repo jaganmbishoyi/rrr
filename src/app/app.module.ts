00575ad8d3135d445082cc73ff44de2bf2b22ec8
Jagan Mohan Bishoyi <jagan.bishoyi@peple10.com>
2022-10-27 18:38:04 +0530

    Http loader and toaster config completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { SharedModule } from "./shared/shared.module";
 import { AppRoutingModule } from "./app-routing.module";
+import { ToastrModule } from "ngx-toastr";
+import { NgHttpLoaderModule } from "ng-http-loader";
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, SharedModule, AppRoutingModule],
+    imports: [
+        BrowserModule,
+        SharedModule,
+        AppRoutingModule,
+        NgHttpLoaderModule.forRoot(),
+        ToastrModule.forRoot({
+            timeOut: 5000,
+            positionClass: "toast-top-right",
+            preventDuplicates: true,
+            closeButton: true,
+            autoDismiss: false,
+        }),
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
