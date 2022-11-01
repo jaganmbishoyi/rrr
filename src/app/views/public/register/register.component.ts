043344c5c0566456ae03a08c23350595a5a08c45
Jagan Mohan Bishoyi <jagan.bishoyi@peple10.com>
2022-11-01 20:50:46 +0530

    Home page design in progress

--- src/app/views/public/register/register.component.ts
+++ src/app/views/public/register/register.component.ts
@@ -21,7 +21,7 @@ export class RegisterComponent implements OnInit {
         },
     ];
 
-    selectedRegisterType: string = "Consumer";
+    selectedRegisterType: string = "Provider";
 
     constructor() {}
 
